0a2712c63ecfa37119c14ca9b7b6dd18f6817eab
Azhanprofessional <amohammedazhan7@gmail.com>
2026-06-15 22:52:11 +0530

    Add files

---
+++ Personalized Greeting Program.py
@@ -0,0 +1,14 @@
+# Personalized Greeting Program
+
+# Step 1: Ask for user details
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+color = input("What is your favorite color? ")
+hobby = input("What's your favorite hobby? ")
+
+# Step 2: Generate a personalized greeting message
+print("\n---- Personalized Greeting ----")
+print(f"Hello, {name}! 👋")
+print(f"You are {age} years old and {color} is a beautiful color!")
+print(f"It's great know that you love {hobby}.")
+print("You're now ready to start your Python adventure 🚀") 
